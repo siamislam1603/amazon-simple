584dd3b11deb5c0387f4ac4d2c6b43e77fc2fd32
Your Name <you@example.com>
2021-11-19 23:16:04 +0600

    Updated design of Header section using Bootstrap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,19 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt="" />
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/manage">Manage Inventory Here</a>
+            <nav className="navbar navbar-expand-lg navbar-black bg-black">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link" href="/shop">Shop</a>
+                        <a className="nav-link" href="/review">Order Review</a>
+                        <a className="nav-link" href="/manage">Manage Inventory Here</a>
+                    </div>
+                    </div>
+                </div>
             </nav>
         </div>
     );
