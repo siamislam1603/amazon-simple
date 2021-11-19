bfd64481c814cf16fc9d49f93508c9ad63768f00
Your Name <you@example.com>
2021-11-19 23:25:28 +0600

    Slightly changed the background color of navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt="" />
-            <nav className="navbar navbar-expand-lg navbar-black bg-black">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
